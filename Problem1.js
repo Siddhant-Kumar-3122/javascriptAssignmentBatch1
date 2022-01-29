7d1357c8c1ca39e36c0f0e658bcebcd0ce2a001a
Siddhant Kumar <89636663+Siddhant-Kumar-3122@users.noreply.github.com>
2022-01-29 17:40:45 +0530

    Add files via upload

---
+++ Problem1.js
@@ -0,0 +1,10 @@
+//Initializing 2 constants with name num1 and num2
+const num1 = 5;
+const num2 = 10;
+
+//Initializing a variable with the product of these two numbers]
+let product1=(num1*num2);
+
+//Logging the product to the console
+console.log(product1);
+
