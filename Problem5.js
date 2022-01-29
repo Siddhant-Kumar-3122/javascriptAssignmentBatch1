de5cdd7f8545e2ee6b5ed4aa2ba2897c0fca6b44
Siddhant Kumar <89636663+Siddhant-Kumar-3122@users.noreply.github.com>
2022-01-29 18:14:25 +0530

    Add files via upload

---
+++ Problem5.js
@@ -0,0 +1,7 @@
+//Initializing the variables
+let a = 0;
+
+//checking for wheather the number is odd or even
+if (a % 2 === 0 && a !== 0) console.log("Even");
+else if (a !== 0) console.log("Odd");
+else console.log("Number is Zero");
