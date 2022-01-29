fa55e2933dd4906bb2e0ce55c0de625152ba1bb4
Siddhant Kumar <89636663+Siddhant-Kumar-3122@users.noreply.github.com>
2022-01-29 17:54:51 +0530

    Add files via upload

---
+++ Problem3.js
@@ -0,0 +1,9 @@
+//Initializing the variables
+let a = 10;
+let b = 20;
+
+//Swapping the two variables using Array Destructuring
+[a, b] = [b, a];
+
+//Logging the swapped variables to the console
+console.log(a, b);
