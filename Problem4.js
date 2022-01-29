8fd57f3c53272841b100f243b5709093095d2b3d
Siddhant Kumar <89636663+Siddhant-Kumar-3122@users.noreply.github.com>
2022-01-29 18:10:17 +0530

    Add files via upload

---
+++ Problem4.js
@@ -0,0 +1,12 @@
+//Initializing the variables
+let a = -10;
+
+//checking for wheather the number is positive, negetive or Zero
+//Logging the output to the console
+if (a > 0) {
+  console.log("Positive");
+} else if (a < 0) {
+  console.log("Negetive");
+} else {
+  console.log("Zero");
+}
