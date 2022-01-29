c38b87b57601de23b11ae5e66f8dc7b00692b87a
Siddhant Kumar <89636663+Siddhant-Kumar-3122@users.noreply.github.com>
2022-01-29 17:47:05 +0530

    Add files via upload

---
+++ Problem2.js
@@ -0,0 +1,9 @@
+//Initializing the constants required for calculating the area of the triangle
+const base = 10;
+const height = 5;
+
+//Calculating the area of the triangle
+let areaOfTriangle = (1 / 2) * base * height;
+
+//Logging the area of the triangle to the console
+console.log(`Area of the Triangle =${areaOfTriangle}`);
